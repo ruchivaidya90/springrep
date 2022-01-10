35396561cd395bbc3c54074360405f8f5fd1fa78
ruchivaidya90 <95929738+ruchivaidya90@users.noreply.github.com>
2022-01-10 14:37:25 +0530

    Add files via upload

---
+++ SpringcoreAssignment1/src/main/java/com/assignment1/Test.java
@@ -0,0 +1,23 @@
+package com.assignment1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+      private static ApplicationContext context;
+
+      public static void main(String[] args) {
+
+            context=new ClassPathXmlApplicationContext("beans.xml");
+
+            Address address=(Address) context.getBean("Address");
+            address.display();
+
+            Customer customer=(Customer) context.getBean("Customer");
+             customer.display();
+             
+             
+         }
+
+}
